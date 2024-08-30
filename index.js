e7ae631fdfd62ba1a07da557dd256f29425b44c0
DESKTOP-5353HBD\plae5 <play56782@gmail.com>
2024-08-30 21:30:20 +0800

    update css && update use localstorage

--- index.js
+++ index.js
@@ -3,8 +3,26 @@ let cartList = document.querySelector(".cart");
 let closeBtn = document.querySelector(".close");
 let counts = document.querySelector(".amount");
 
+let product = null;
+let buyList = [];
+
+//頁面載入先抓bowser的資料
+document.addEventListener('DOMContentLoaded',()=>{
+    let saveUsertext=localStorage.getItem('filterProductList');
+    let saveUserCount=localStorage.getItem('totalCount');
+    if(saveUsertext){
+        //解析json檔轉換回陣列
+        buyList=JSON.parse(saveUsertext);
+        addCart();
+    }
+    if(saveUserCount){
+        //恢復購買數量並顯示
+        counts.innerText=saveUserCount;
+    }
+})
+
 clickCart.addEventListener("click", () => {
-    if (cartList.style.right == "-100%") {
+    if (cartList.style.right == "-100%" || cartList.style.right =='') {
         cartList.style.right = "0";
     } else {
         cartList.style.right = "-100%";
@@ -14,8 +32,6 @@ closeBtn.addEventListener("click", () => {
     cartList.style.right = "-100%";
 });
 
-let product = null;
-let buyList = [];
 //取json檔
 fetch('product.json')
     .then(response => response.json())
@@ -41,22 +57,23 @@ function showProduct() {
         });
     }
 }
-
+//條件判斷
 function buy(productID){
     let thisProduct = product.find(product=>product.id===productID);
+    let cartArray = buyList.find(item=>item.id===productID);
     //商品不在購物車清單中
-    if(!buyList[productID]){
+    if(!cartArray){
         //無重複情況下顯示預設
-        buyList[productID]=thisProduct;
-        buyList[productID].count=1;
+        buyList.push(thisProduct)
+        thisProduct.count=1;
     }else{
         //重複則加數量
-        buyList[productID].count++;
+        cartArray.count++;
     }
     addCart();
 }
+//加入購物車
 function    addCart(){
-    //加入購物車
     let listCart = document.querySelector('.listCart');
     listCart.innerHTML=''
     let totalCount = 0;
@@ -82,22 +99,26 @@ function    addCart(){
         })
         counts.innerText=totalCount;
         //過濾null及undefind的值
-        let filterProductList = buyList.filter(product=>product!=null || product!=undefined);
+        let filterProductList = buyList.filter(product=>product!==null && product!==undefined);
         //儲存至bowser
         localStorage.setItem('filterProductList',JSON.stringify(filterProductList));
+        localStorage.setItem('totalCount',totalCount);
 }
-
+//+ or - function
 function changeAmount(productID,$type){
-    switch ($type){
-        case "-":
-            buyList[productID].count--;
-        break;
-        case "+":
-            buyList[productID].count++;
-        break;
-    }
-    if(buyList[productID].count<=0){
-        delete buyList[productID];
-    }
+    const selectThisProduct = buyList.findIndex(product=>product && product.id===productID)
+        if(selectThisProduct!==-1){
+            switch ($type){
+                case "-":
+                    buyList[selectThisProduct].count--;
+                break;
+                case "+":
+                    buyList[selectThisProduct].count++;
+                break;
+            }
+        }
+        if(buyList[selectThisProduct].count<=0){
+            buyList.splice(selectThisProduct,1)
+        }
     addCart();
 }
